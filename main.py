b1da651e3fbaa8c093b0c7737447493e4a70ddf3
Mustak sk <91077142+Mustak9009@users.noreply.github.com>
2024-08-29 17:33:58 +0530

    Listen audio

--- main.py
+++ main.py
@@ -4,6 +4,24 @@ import pyttsx3
 recognizer = sr.Recognizer();
 engine = pyttsx3.init()
 
-if __name__ == '__main__':
-    engine.say("Project Description: A user-friendly application designed to [insert primary function or purpose here], featuring intuitive interfaces and robust functionality to enhance user experience and efficiency.")
+def sayCommand(text):
+    engine.say(text)
     engine.runAndWait()
+
+
+if __name__ == '__main__':
+    sayCommand("Initializing Jarvis . . .")
+    
+    while True:
+        # obtain audio from the microphone
+        r = sr.Recognizer()
+        print("Recognizing...")
+        try:
+            with sr.Microphone() as source:
+                print("Say something...")
+                audio = r.listen(source,timeout=2)
+            command = r.recognize_google(audio)
+            print(command)
+            sayCommand(command)
+        except Exception as e:
+            print("An error occured; {0}".format(e))
