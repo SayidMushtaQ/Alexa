1a6a9cdfebe622228dd9da130cc3f94376cb9558
Mustak sk <91077142+Mustak9009@users.noreply.github.com>
2024-08-30 11:12:39 +0530

    Follow initial command

--- main.py
+++ main.py
@@ -1,16 +1,21 @@
 import speech_recognition as sr
 import pyttsx3
-
+import webbrowser
 recognizer = sr.Recognizer();
 engine = pyttsx3.init()
 
 def sayCommand(text):
     engine.say(text)
     engine.runAndWait()
-
+def processCommand(command:str):
+    if 'open youtube and search' in command.lower():
+        search_query = command.split('search')[1].strip()
+        print(search_query)
+        print(command)
+        webbrowser.open_new(f'https://www.youtube.com/results?search_query={search_query.replace(' ','+')}')
 
 if __name__ == '__main__':
-    sayCommand("Initializing alexa . . .")
+    sayCommand("Initializing Jarvis")
     
     while True:
         # obtain audio from the microphone
@@ -18,11 +23,17 @@ if __name__ == '__main__':
         print("Recognizing...")
         try:
             with sr.Microphone() as source:
-                print("Say something...")
+                print("Listening . . .")
                 audio = r.listen(source,timeout=2,phrase_time_limit=3)
-            command = r.recognize_google(audio)
-            if(command.lower() == 'alexa'):
+            word = r.recognize_google(audio)
+            if(word.lower() == 'jarvis'):
                 sayCommand("Yess boss!!")
+                with sr.Microphone() as source:
+                    print("Jarvis Activated ...")
+                    audio = r.listen(source,timeout=2);
+                    command = r.recognize_google(audio)
+                    processCommand(command)
+
+
         except Exception as e:
-            print(e)
             print("An error occured; {0}".format(e))
