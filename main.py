5e0b895c9e1ae6a46fab1c0576d0360bade5402a
Mustak sk <91077142+Mustak9009@users.noreply.github.com>
2024-09-01 12:02:55 +0530

    add

--- main.py
+++ main.py
@@ -3,32 +3,50 @@ import pyttsx3
 import webbrowser
 from musicLibrary import musics
 import requests
+from openaiChat import chatWithGPT,generateIMG
+from gtts import gTTS
+import pygame
+import os
+pygame.mixer.init()
 
-
-recognizer = sr.Recognizer();
+recognizer = sr.Recognizer();       
 engine = pyttsx3.init()
 
-def sayCommand(text):
+def say_old(text):
     engine.say(text)
     engine.runAndWait()
+def say(text):
+    tts = gTTS(text)
+    tts.save('temp.mp3')
+    
+    pygame.mixer.music.load('temp.mp3')
+    pygame.mixer.music.play()
+
+    while pygame.mixer.music.get_busy():
+        pygame.time.Clock().tick(10)
+
+    pygame.mixer.music.unload()
+    os.remove('temp.mp3')
+        
 def processCommand(command:str):
 
     if 'open youtube and search' in command.lower():
         search_query = command.split('search')[1].strip()
         print(search_query)
-        sayCommand(f"Searching {search_query} on yoututbe")
+        say(f"Searching {search_query} on yoututbe")
         webbrowser.open_new(f'https://www.youtube.com/results?search_query={search_query.replace(' ','+')}')
     elif 'open youtube' in command.lower():
-        sayCommand("opening youTube")
+        say("opening youTube")
         webbrowser.open('https://www.youtube.com/')
     elif 'open facebook' in command.lower():
-        sayCommand("Opening facebook")
-        webbrowser.open('https://www.likdin.com/')
+        say("Opening facebook")
+        webbrowser.open('https://www.facebook.com/')
     elif command.lower().startswith('play'):
         song_req = command.split('play')[1].strip();
         serch_song = musics.get(song_req);
+        print(serch_song)
         webbrowser.open(serch_song)
-        sayCommand(f"Playing {song_req}")
+        say(f"Playing")
     elif command.lower().startswith('news'):
         news_req = command.split('news')[1].strip();
         news_from_API = 'https://newsapi.org/v2/top-headlines?country={}&apiKey=14dea05a5f53437e8c827a5577223a63';
@@ -43,14 +61,20 @@ def processCommand(command:str):
             articles = data.get('articles')
             for article in articles:
                 print(article.get('title'))
-                sayCommand(article.get('title'))
+                say(article.get('title'))
         else:
             print(f"Request failed with status code {response.status_code}")
+    elif command.lower().startswith('create image'):
+        say("creating image ...")
+        output = generateIMG(command)
+        webbrowser.open(output)
     else:
-        pass
+        output = chatWithGPT(command)
+        print(output)
+        say(output)
 
 if __name__ == '__main__':
-    sayCommand("Initializing Jarvis")
+    say("Initializing alexa")
     
     while True:
         # obtain audio from the microphone
@@ -61,10 +85,10 @@ if __name__ == '__main__':
                 print("Listening . . .")
                 audio = r.listen(source,timeout=2,phrase_time_limit=3)
             word = r.recognize_google(audio)
-            if(word.lower() == 'jarvis'):
-                sayCommand("Yess boss!!")
+            if(word.lower() == 'alexa'):
+                say("Yess boss")
                 with sr.Microphone() as source:
-                    print("Jarvis Activated ...")
+                    print("Alexa Activated ...")
                     audio = r.listen(source,timeout=2,phrase_time_limit=3);
                     command = r.recognize_google(audio)
                     processCommand(command)
